cc20760ead5092cc2c0d500895c444ccb8604955
Aulia Hakiem <noersedya@gmail.com>
2022-06-19 17:10:06 +0700

    enable packages on jupyter.nix

--- jupyter.nix
+++ jupyter.nix
@@ -15,9 +15,9 @@ mkShell {
       ipython
 
       # Uncomment the following lines to make them available in the shell.
-      # pandas
-      # numpy
-      # matplotlib
+      pandas
+      numpy
+      matplotlib
     ]))
   ];
 
